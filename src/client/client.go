f9e8b98cc315ef10baf9954677e0c2d85ea019ed
root <413735844@qq.com>
2020-09-05 15:44:56 +0800

    提交第一版的client.go

---
+++ src/client/client.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+	"encoding/binary"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial( "tcp", "127.0.0.1:3563" )
+	if err != nil {
+		panic( err )
+	}
+	data := []byte( `{
+		"Register":{
+			"Account":"xiecheng",
+			"PassWord":"123456"
+		}
+	}` )
+
+	m := make( []byte, 2 + len( data ) )
+
+	binary.BigEndian.PutUint16( m, uint16( len( data ) ) )
+	copy( m[2:], data )
+	conn.Write( m )
+}
